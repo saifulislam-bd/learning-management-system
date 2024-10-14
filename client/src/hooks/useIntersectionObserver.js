5d7020f66d83029927e6aa3a7a4ae9216e9fde42
saifulislam-bd <saifulislam.github@gmail.com>
2024-10-14 18:49:25 +0600

    add useIntersectionObserver custom hook

---
+++ client/src/hooks/useIntersectionObserver.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+function useIntersectionObserver(ref, options) {
+  const [intersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting);
+    }, options);
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, options]);
+
+  return intersecting;
+}
+
+export default useIntersectionObserver;
